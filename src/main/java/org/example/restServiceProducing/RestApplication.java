88e2114668fbee11d6731f57f71c7789d942a794
Ivan_Demydko <ivandemydko@gmail.com>
2019-12-18 13:06:35 +0200

    add ApplicationRunner

---
+++ src/main/java/org/example/restServiceProducing/RestApplication.java
@@ -0,0 +1,12 @@
+package org.example.restServiceProducing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+}
